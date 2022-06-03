8019596f8cb2f3e433061718636e88a4fef8c72e
jangkyoungtae <zxs3710@naver.com>
2022-06-04 00:30:24 +0900

    add Mock Api URL 변경

--- src/api/wordApi.tsx
+++ src/api/wordApi.tsx
@@ -2,5 +2,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "http://localhost:3000/api",    
+    baseURL: "http://wordlist.s3-website.ap-northeast-2.amazonaws.com/api",    
 })
