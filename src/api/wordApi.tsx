cd6c1798fb80af0b0372b5e62293d9eea03cb817
jangkyoungtae <zxs3710@naver.com>
2022-06-06 11:26:31 +0900

    Mack api 호출 url 배포용으로 변경 (api 추가 안해줘서 에러 남 해결)

--- src/api/wordApi.tsx
+++ src/api/wordApi.tsx
@@ -2,5 +2,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "wordlist.s3-website.ap-northeast-2.amazonaws.com",
+    baseURL: "http://wordlist.s3-website.ap-northeast-2.amazonaws.com/api",
 })
